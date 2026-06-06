42ad86341d3c72f5085f368a21fe14d3d7ecc5bb
FidanUmudova <fidanumudova05@gmail.com>
2026-06-06 16:20:54 +0400

    first commit

---
+++ models.py
@@ -0,0 +1,27 @@
+import sqlite3 as sql
+from os import path
+
+ROOT = path.dirname(path.abspath(__file__))
+
+def create_post(name, content):
+    con = sql.connect(path.join(ROOT, 'database.db'))
+    cur = con.cursor()
+
+    cur.execute(
+        'INSERT INTO posts (name, content) VALUES (?, ?)',
+        (name, content)
+    )
+
+    con.commit()
+    con.close()
+
+
+def get_posts():
+    con = sql.connect(path.join(ROOT, 'database.db'))
+    cur = con.cursor()
+
+    cur.execute('SELECT * FROM posts')
+    posts = cur.fetchall()
+
+    con.close()
+    return posts
